73b370e5c43e6d5f886dd15aa5140ae2785f57d3
Mitchell Kember <mk12360@gmail.com>
2016-11-12 15:01:57 -0800

    Add pydeps.sh script to pip install dependencies

---
+++ pydeps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+command -v python &>/dev/null || { echo "python not found"; exit 1; }
+
+version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+if [[ $version != "2.7" ]]; then
+	echo "your python is version $version, but we need 2.7"
+	exit 1
+fi
+
+pip install python-dateutil amadeus nltk simplejson && echo "SUCCESS"
