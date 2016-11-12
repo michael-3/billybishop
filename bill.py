e31bd66327b47609bc2b511d7833249d4bb12538
tclinken <trevor1515@gmail.com>
2016-11-12 04:11:05 -0800

    Created very simple Flask server

--- bill.py
+++ bill.py
@@ -1,6 +1,19 @@
-from flask import Flask
+#!/usr/bin/python
+
+from flask import Flask, render_template, request
+from flight_finder import find_flights
+import simplejson as json
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def main_app():
-	return 'Billy Bishop Chatbot'
+  return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def login():
+  return json.dumps(find_flights(request.args.get('destination')))
+
+if __name__ == '__main__':
+  app.run()
