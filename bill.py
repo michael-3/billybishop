29ff5a9f3d16ff8e2e798fd630588a91bf0e0092
Mitchell Kember <mk12360@gmail.com>
2016-11-12 14:12:18 -0800

    Use /usr/bin/env python shebang

--- bill.py
+++ bill.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from flask import Flask, render_template, request
 from flight_finder import find_flights
--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import dateutil.parser as dparser
 from nltk.tokenize.mwe import MWETokenizer
