8bd3a6e893b358a7397d72991fa7db231dc36b85
Mitchell Kember <mk12360@gmail.com>
2016-11-12 14:36:43 -0800

    Make parser.py conform to PEP 8

--- parser.py
+++ parser.py
@@ -5,36 +5,39 @@ import dateutil.parser as dparser
 from nltk.tokenize.mwe import MWETokenizer
 import csv
 
+
 def load_city_tokens():
-  """Loads tokens to use with MWETokenizer from cities.csv."""
-  tokens = []
-  with open('cities.csv') as f:
-    for (city, _) in csv.reader(f):
-	tokens.append(tuple(city.split('_')))
-  print tokens
-  return tokens
+    """Loads tokens to use with MWETokenizer from cities.csv."""
+    tokens = []
+    with open('cities.csv') as f:
+        for (city, _) in csv.reader(f):
+            tokens.append(tuple(city.split('_')))
+    return tokens
+
 
 CITY_TOKENS = load_city_tokens()
 
-# returns a map of {'origin' : departure_location, 'destination' : arrival_location}
+
 def get_route(msg):
-	tokenizer = MWETokenizer(CITY_TOKENS)
-	route = {'origin' : None, 'destination' : None}
-	tokens = tokenizer.tokenize(msg.split(' '))
-	for i in xrange(len(tokens) - 1):
-		# fuzzy matching here?
-		if tokens[i] == 'from':
-			route['origin'] = tokens[i+1]
-		if tokens[i] == 'to':
-			route['destination'] = tokens[i+1]
-	return route
+    """Returns a map with keys 'origin'" and 'arrival_location'."""
+    tokenizer = MWETokenizer(CITY_TOKENS)
+    route = {'origin': None, 'destination': None}
+    tokens = tokenizer.tokenize(msg.split(' '))
+    for i in xrange(len(tokens) - 1):
+        # fuzzy matching here?
+        if tokens[i] == 'from':
+            route['origin'] = tokens[i+1]
+        if tokens[i] == 'to':
+            route['destination'] = tokens[i+1]
+    return route
+
 
 def get_departure(msg):
-	date_time = dparser.parse(msg, fuzzy=True, yearfirst=True)
-	departure = {'departure' : str(date_time.date())}
-	return departure
-	
+    date_time = dparser.parse(msg, fuzzy=True, yearfirst=True)
+    departure = {'departure': str(date_time.date())}
+    return departure
+
 
 if __name__ == '__main__':
-	msg = sys.argv[1]
-	print get_route(msg), get_departure(msg)
+    msg = sys.argv[1]
+    print get_route(msg), get_departure(msg)
