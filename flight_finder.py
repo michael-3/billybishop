383bfae786994276f5f09273c14b331b250b80dd
tclinken <trevor1515@gmail.com>
2016-11-13 08:42:45 -0800

    Properly capitalize city names in messages

--- flight_finder.py
+++ flight_finder.py
@@ -75,9 +75,9 @@ def reask(departure_date, destination, origin):
     if (not departure_date) and (not destination) and (not origin):
         response['message'] = 'Let us know your travel plans!'
     elif (not departure_date) and (not destination) and origin:
-        response['message'] = 'Where do you want to go from {0}, and when are you leaving?'.format(origin)
+        response['message'] = 'Where do you want to go from {0}, and when are you leaving?'.format(origin.title())
     elif (not departure_date) and destination and (not origin):
-        response['message'] = 'Nice, {0} is great this time of year! When are you leaving, and where from?'.format(destination)
+        response['message'] = 'Nice, {0} is great this time of year! When are you leaving, and where from?'.format(destination.title())
     elif departure_date and (not destination) and (not origin):
         response['message'] = 'That\'s a good time for a trip! Where are you travelling between?'
     elif (not departure_date) and destination and origin:
@@ -85,7 +85,7 @@ def reask(departure_date, destination, origin):
     elif departure_date and (not destination) and origin:
         response['message'] = 'Where are you planning to go?'
     elif departure_date and destination and (not origin):
-        response['message'] = 'That\'s a great time to visit {0}! Where will you be departing from?'.format(destination)
+        response['message'] = 'That\'s a great time to visit {0}! Where will you be departing from?'.format(destination.title())
 
     if departure_date:
         response['departure_date'] = departure_date
@@ -98,6 +98,6 @@ def reask(departure_date, destination, origin):
 
     return response
 
-# for testing (for example: ./flight_finder.py BOM | python -m json.tool)
+# for testing (for example: ./flight_finder.py "I'm going from Rome to Paris on 2016-12-12" | python -m json.tool)
 if __name__ == '__main__':
     print json.dumps(find_flights(sys.argv[1]))
